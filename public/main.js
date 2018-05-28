431794ff8bcb8e24e05cb4f38d635ce7aa564a92
talhaker <taltal.haker@gmail.com>
2018-05-28 23:16:50 +0300

    Get posts from DB and display

--- public/main.js
+++ public/main.js
@@ -1,10 +1,26 @@
 import PostsRepository from './posts-repository.js';
 import PostsRenderer from './posts-renderer.js';
-import EventsHandler from './events-handler.js'; 
+import EventsHandler from './events-handler.js';
 
 let postsRepository = new PostsRepository();
 let postsRenderer = new PostsRenderer();
 let eventsHandler = new EventsHandler(postsRepository, postsRenderer);
+
+// Request the /posts server route
+let getPosts = function() {
+    $.ajax({
+        method: "GET",
+        url: '/posts',
+        success: function(posts) {
+            postsRenderer.renderPosts(posts);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log(textStatus);
+        }
+    });
+}
+
+getPosts();
 
 eventsHandler.registerAddPost();
 eventsHandler.registerRemovePost();
